adec65dcb8be3433b306e4258a10223c6d12c817
Caio <39690305+caiovini@users.noreply.github.com>
2019-07-06 17:15:31 -0300

    Update
    Change fixed IP to localhost

--- src/components/IPFS/ipfs.js
+++ src/components/IPFS/ipfs.js
@@ -1,13 +1,13 @@
 
 const ipfsClient = require('ipfs-http-client'); // ipfs api
 
-const ipfs = ipfsClient({ host: '169.254.209.107', 
+const ipfs = ipfsClient({ host: 'localhost', 
                           port: 5001 , 
                           protocol: 'http',
                           EXPERIMENTAL: {
                             pubsub: true
                           }});
 
-const baseLink = 'http://169.254.209.107:9090/ipfs/';                          
+const baseLink = 'http://localhost:9090/ipfs/';                          
 
 module.exports = {ipfs , baseLink}; 
